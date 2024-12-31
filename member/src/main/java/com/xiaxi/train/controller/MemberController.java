9aaffd0882eb741336b3e8c2ea2f92ae6d65d5cf
夏溪 <2510678695@qq.com>
2024-12-31 11:59:52 +0800

    第三次修改

--- member/src/main/java/com/xiaxi/train/controller/MemberController.java
+++ member/src/main/java/com/xiaxi/train/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.xiaxi.train.controller;
 
 import com.xiaxi.common.response.ApiRestResponse;
+import com.xiaxi.train.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,13 +10,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-public class UserController {
+public class MemberController {
+    @Autowired
+    private MemberService memberService;
 
     @GetMapping("/")
     public ApiRestResponse welcome() {
         Map<String, Object> map = new HashMap<>();
         map.put("idNumber", "123456789012345678");
         map.put("result","欢迎访问会员服务");
+        map.put("mobile", memberService.getMemberMobileById(1L));
 
         return ApiRestResponse.success(map);
     }
